e4ad13357e2e7127f1bb75780afbbe6ce2f7d221
Rob Cole <robcole@gmail.com>
2015-12-10 19:48:01 -0800

    Update vagrant/nginx config files
    Updates vagrant build process to ensure nginx is propertly
    restarted after copying new site configs in place. Updates
    package.json to valid JSON.

--- chef/cookbooks/imo/scripts/after_provision.sh
+++ chef/cookbooks/imo/scripts/after_provision.sh
@@ -5,6 +5,7 @@ FILESDIR=/home/vagrant/imo/chef/cookbooks/imo/files
 # Clean all seutp files
 sudo rm -f /etc/postgresql/9.4/main/pg_hba.conf
 sudo rm -f /etc/nginx/sites-available/imo
+sudo rm -f /etc/nginx/sites-available/default
 sudo rm -f /etc/nginx/sites-enabled/imo
 sudo rm -f /etc/nginx/sites-enabled/default
 # sudo rm -rf /etc/ssl/imo.dev
@@ -15,7 +16,7 @@ cp $FILESDIR/pg_hba.conf /etc/postgresql/9.4/main/pg_hba.conf
 service postgresql start
 
 # Add SSL Support & add imo to nginx
-# service nginx stop
+service nginx stop
 # mkdir /etc/ssl/imo.dev
 # cd /etc/ssl/imo.dev
 
